34f04e935116588a2c81ce0080835b94684abd3e
CarolinaArboleda <carolina.arboledaa@upb.edu.co>
2022-08-11 08:55:32 -0500

    Working task 3 and 4

--- task4.cpp
+++ task4.cpp
@@ -3,6 +3,39 @@
 
 
 void task4(){
+    num class Task3States{
+        INIT,
+        WAIT_TO_TOGGLE_LED
+    };
+    static Task4States task4State = Task4States::INIT;
+    static uint32_t lasTime;
+    static constexpr uint32_t INTERVAL = 250;
+    static constexpr uint8_t ledYellow = 4;
+    static bool ledStatus = false;
+
+    switch(task4State){
+        case Task4States::INIT:{
+            pinMode(ledYellow,OUTPUT);
+            lasTime = millis();
+            task4State = Task4States::WAIT_TO_TOGGLE_LED;
+            break;
+        }
+
+        case Task4States::WAIT_TO_TOGGLE_LED:{
+            // evento 1:
+            uint32_t currentTime = millis();
+            if( (currentTime - lasTime) >= INTERVAL ){
+                lasTime = currentTime;
+                digitalWrite(ledYellow,ledStatus);
+                ledStatus = !ledStatus;
+            }
+            break;
+        }
+
+        default:{
+            break;
+        }
+    }
 
     
 }
